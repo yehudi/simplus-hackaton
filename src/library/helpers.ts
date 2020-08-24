62989904d7adb09a4919f6485b142dfef83e1e4b
@jamangile <athenkosi@hotmail.co.za>
2020-08-24 10:01:37 +0200

    * updated: testOverload function

--- src/library/helpers.ts
+++ src/library/helpers.ts
@@ -1,7 +1,7 @@
 import { Breadcrumb, vehicleConfig } from "./models";
 
-export const testOverload = (config: vehicleConfig, data: Breadcrumb[]) => {
+export const testOverload = (config: vehicleConfig, data: Breadcrumb) => {
     const bridge = (config.length * 2.1) + 18
     const limit = Math.min(config.max_weight, bridge)
-    return data[0].gross_payload > limit
+    return data.gross_payload > limit
 }
